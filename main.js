133d295958aa126a3aee5d8cca7090426da5ae43
Remi Slanina <remislanina@gmail.com>
2025-07-31 14:36:52 +0200

    upward movement implemented

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ ctx.imageSmoothingEnabled = false;
 canvas.style.imageRendering = "crisp-edges";
 let canvasWidth;
 let canvasHeight;
-let scrollSpeed = 12;
+let scrollSpeed = 1;
 //let globalFrameCount = 0;
 
 // ** SPRITES **
@@ -39,6 +39,7 @@ class Sprite {
     this.scale = options.scale || 1;
     this.x = options.x || 0;
     this.y = options.y || 0;
+    this.initialY = this.y;
 
     this.states = options.states || ["default"];
     this.currentState = options.startState || this.states[0];
@@ -48,6 +49,24 @@ class Sprite {
     this.frameX = 0;
     this.frameStaggerRate = options.frameStaggerRate || 5;
     this.frameCounter = 0;
+
+    /* for butterflies only */
+    this.flutter = options.flutter || false;
+
+    /* random movement speed: 0.5 base this.speed, 
+    + variation between 0 - 0.5  */
+    /*     this.speed = 0.5 + Math.random() * 0.5;
+    this.angle = Math.random() * Math.PI * 2; // For sine movement
+    this.angleSpeed = 0.02 + Math.random() * 0.02;
+    this.offset = Math.random() * 100;  */
+
+    // New movement = FLUTTER UP <3
+    this.rising = options.rising || false;
+    this.riseSpeed = options.riseSpeed || 0.2 + Math.random() * 0.3;
+    this.riseDirection = options.riseDirection || (Math.random() - 0.5) * 2; // horizontal drift
+    this.maxRise = options.maxRise || 500;
+    this.angle = Math.random() * Math.PI * 2;
+    this.angleSpeed = 0.02 + Math.random() * 0.02;
   }
   slowDown() {
     if (this.frameStaggerRate < 20) this.frameStaggerRate++;
@@ -63,10 +82,28 @@ class Sprite {
       this.frameCounter = 0;
     }
   }
-  update() {
+  update(deltaTime) {
     this.frameCounter++;
     if (this.frameCounter % this.frameStaggerRate === 0) {
       this.frameX = (this.frameX + 1) % (this.maxFrames + 1);
+    }
+    /* if (this.flutter) {
+      this.angle += this.angleSpeed;
+      this.y += Math.sin(this.angle) * this.speed;
+      this.x += Math.cos(this.angle + this.offset) * this.speed * 0.5;
+    } */
+
+    if (this.flutter) {
+      this.angle += this.angleSpeed;
+      this.y += Math.sin(this.angle) * 0.3;
+      this.x += Math.cos(this.angle + this.angleSpeed) * 0.2;
+    }
+
+    if (this.rising) {
+      this.y -= this.riseSpeed; // upward
+      this.x += this.riseDirection * 0.3; // slight sideways drift
+      //stop after maxRise
+      if (this.initialY - this.y > this.maxRise) this.rising = false;
     }
   }
   draw(ctx) {
@@ -112,7 +149,7 @@ const snail = new Sprite({
   x: Math.floor(canvasWidth / 2 - 215 / 2),
   y: canvasHeight - 130,
   scale: 1,
-  frameStaggerRate: 5,
+  frameStaggerRate: 6,
   maxFrames: 8, //actually 0-8
   states: ["normal", "curled", "butterfly", "ghost"],
   startState: "normal",
@@ -124,10 +161,11 @@ const butterfly = new Sprite({
   x: Math.floor(canvasWidth / 2 - 100 / 2),
   y: canvasHeight / 2,
   scale: 0.7,
-  frameStaggerRate: 5,
+  frameStaggerRate: 1,
   maxFrames: 6, //acutally 0-6
   states: ["white", "black", "color"],
   startState: "white",
+  flutter: true,
 });
 
 // BUTTERFLY SWARMS
@@ -151,17 +189,53 @@ function createSwarm(num, ...colors) {
         x: xb,
         y: yb,
         scale: 0.7,
-        frameStaggerRate: 5,
+        frameStaggerRate: 11,
         maxFrames: 6, //acutally 0-6
         states: ["white", "black", "color"],
         startState: color,
+        flutter: true,
       })
     );
   }
   return bswarm;
 }
-const swarm = createSwarm(10, "white", "black", "color");
+//const swarm = createSwarm(10, "white", "black", "color");
 
+function spawnButterflySwarmFromShell(shellX, shellY, count = 10) {
+  const bswarm = [];
+
+  /*   b.x =
+        snail.x +
+        (snail.spriteWidth * snail.scale) / 2 +
+        (Math.random() - 0.5) * 120 -
+        30; // -30 to position it slightly towards the shell
+      b.y = snail.y + (Math.random() - 0.5) * 20;
+ */
+
+  for (let i = 0; i < count; i++) {
+    const offsetX = (Math.random() - 0.5) * 30;
+    const offsetY = (Math.random() - 0.5) * 20;
+
+    bswarm.push(
+      new Sprite({
+        src: "butterfly-sprite-01.svg",
+        spriteWidth: 100,
+        spriteHeight: 80,
+        x: shellX / 2 + offsetX - 30,
+        y: shellY + offsetY,
+        scale: 0.6,
+        frameStaggerRate: 15,
+        maxFrames: 6,
+        flutter: true,
+        rising: true,
+        riseSpeed: 0.3 + Math.random() * 0.3,
+        maxRise: 150 + Math.random() * 50,
+      })
+    );
+  }
+  return bswarm;
+}
+const swarm = spawnButterflySwarmFromShell(snail.x, snail.y);
 canvas.addEventListener("click", () => {
   const nextIndex =
     (snail.states.indexOf(snail.currentState) + 1) % snail.states.length;
@@ -178,6 +252,8 @@ const parallaxLayer3 = new Image();
 parallaxLayer3.src = "fibonacci-mountain-bg-03.svg";
 const parallaxLayer4 = new Image();
 parallaxLayer4.src = "fibonacci-mountain-bg-04.svg";
+const nonParallaxLayer1 = new Image();
+nonParallaxLayer1.src = "fibonacci-bg-non-parallax.svg";
 
 // background layer class for parallax effects:
 class Layer {
@@ -241,12 +317,13 @@ class Layer {
 }
 
 // ** PARALLAX **
-const layer1 = new Layer(parallaxLayer1, 0.2);
-const layer2 = new Layer(parallaxLayer2, 0.4);
-const layer3 = new Layer(parallaxLayer3, 0.6);
-const layer4 = new Layer(parallaxLayer4, 0.8);
+const pLayer1 = new Layer(parallaxLayer1, 0.25);
+const pLayer2 = new Layer(parallaxLayer2, 0.3);
+const pLayer3 = new Layer(parallaxLayer3, 0.35);
+const pLayer4 = new Layer(parallaxLayer4, 0.4);
+const npLayer1 = new Layer(nonParallaxLayer1, 0);
 
-const parallaxLayers = [layer1, layer2, layer3, layer4];
+const parallaxLayers = [pLayer1, pLayer2, pLayer3, pLayer4, npLayer1];
 
 //slow down the parallax background
 function slowDownscrollSpeed() {
@@ -314,11 +391,22 @@ function resizeCanvas() {
   }
 
   // insert swarm class maybe tomorrow ...? Let the swarm manage itself
+  //insert rise logic here, too
+  // TO DO rise
   if (swarm && Array.isArray(swarm)) {
     swarm.forEach((b, i) => {
       //scatter butterflies randomly
+      /* 
       b.x = Math.random() * canvasWidth;
       b.y = Math.random() * canvasHeight * 0.6;
+       */
+
+      b.x =
+        snail.x +
+        (snail.spriteWidth * snail.scale) / 2 +
+        (Math.random() - 0.5) * 120 -
+        30; // -30 to position it slightly towards the shell
+      b.y = snail.y + (Math.random() - 0.5) * 20;
     });
   }
 }
@@ -397,8 +485,8 @@ function animate() {
     });
   }
   /* debug */
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, canvasHeight - 1, canvasWidth, 1); // bottom red line
+  //ctx.fillStyle = "red";
+  //ctx.fillRect(0, canvasHeight - 1, canvasWidth, 1); // bottom red line
   /* debug */
   requestAnimationFrame(animate);
 }
